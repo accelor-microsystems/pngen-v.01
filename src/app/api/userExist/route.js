aefe10037a7f6ecb6648a846d79b51c68c0871bb
accelor-microsystems <rnd@accelorindia.com>
2024-06-03 16:57:36 +0530

    Update route.js

--- src/app/api/userExist/route.js
+++ src/app/api/userExist/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectMongoDB from "../../../libs/mongodb";
+import connectMongoDB from "../../../lib/mongodb";
 import User from "../../../models/user";
 import bcrypt from 'bcrypt'
 import { cookies } from 'next/headers'
