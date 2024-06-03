4d4a768db563de1e37c6ef46b7bdef1e084ae4c5
accelor-microsystems <rnd@accelorindia.com>
2024-06-03 16:57:27 +0530

    Update route.js

--- src/app/api/table/route.js
+++ src/app/api/table/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectMongoDB from "../../../libs/mongodb";
+import connectMongoDB from "../../../lib/mongodb";
 import MPN from "../../../models/mpn";
 
 
